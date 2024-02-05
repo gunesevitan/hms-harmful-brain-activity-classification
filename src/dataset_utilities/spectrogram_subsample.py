3e9a32c0af67d533bfd3a6be7f9fd09a323cebe0
gunesevitan <gunesevitan@gmail.com>
2024-02-05 17:30:12 +0300

    spec subsample

--- src/dataset_utilities/spectrogram_subsample.py
+++ src/dataset_utilities/spectrogram_subsample.py
@@ -8,6 +8,54 @@ sys.path.append('..')
 import settings
 
 
+def get_spectrogram(df_spectrogram, start_time, fill_na=False, log_scale=False):
+
+    """
+    Get spectrogram subsample array from the given dataframe
+
+    Parameters
+    ----------
+    df_spectrogram: pandas.DataFrame
+        Dataframe with time and signal columns
+
+    start_time: int
+        Spectrogram offset seconds
+
+    fill_na: bool
+        Whether to fill missing values or not
+
+    log_scale: bool
+        Whether to do log transform or not
+
+    Returns
+    -------
+    spectrogram: numpy.ndarray of shape (n_frequencies, n_time_steps, n_channels)
+        Array of spectrogram
+    """
+
+    if start_time % 2 == 0:
+        start_time += 1
+    end_time = start_time + 598
+
+    df_spectrogram_subsample = df_spectrogram.loc[(df_spectrogram['time'] >= start_time) & (df_spectrogram['time'] <= end_time)].iloc[:, 1:]
+
+    if fill_na:
+        df_spectrogram_subsample = df_spectrogram_subsample.fillna(0)
+
+    spectrogram = df_spectrogram_subsample.values.T
+    spectrogram = np.stack((
+        spectrogram[0:100, :],
+        spectrogram[100:200, :],
+        spectrogram[200:300, :],
+        spectrogram[300:400, :],
+    ), axis=-1)
+
+    if log_scale:
+        spectrogram = np.log1p(spectrogram)
+
+    return spectrogram
+
+
 if __name__ == '__main__':
 
     dataset_directory = settings.DATA / 'spectrogram_subsample'
@@ -20,52 +68,17 @@ if __name__ == '__main__':
     df_train = pd.read_csv(settings.DATA / 'hms-harmful-brain-activity-classification' / 'train.csv')
     metadata = []
 
-    channels = False
-
     for spectrogram_id, df_train_spectrogram in tqdm(df_train.groupby('spectrogram_id'), total=df_train['spectrogram_id'].nunique()):
 
         df_spectrogram = pd.read_parquet(spectrogram_directory / f'{spectrogram_id}.parquet')
-        spectrogram_columns = df_spectrogram.columns.tolist()
-        left_temporal_chain = [column for column in spectrogram_columns if column.startswith('LL')]
-        right_temporal_chain = [column for column in spectrogram_columns if column.startswith('RL')]
-        left_parasagittal_chain = [column for column in spectrogram_columns if column.startswith('LP')]
-        right_parasagittal_chain = [column for column in spectrogram_columns if column.startswith('RP')]
 
         for _, row in df_train_spectrogram.iterrows():
 
             spectrogram_sub_id = row['spectrogram_sub_id']
             start_time = int(row['spectrogram_label_offset_seconds'])
 
-            if start_time % 2 == 0:
-                start_time += 1
-            end_time = start_time + 598
-
-            df_spectrogram_subsample = df_spectrogram.loc[(df_spectrogram['time'] >= start_time) & (df_spectrogram['time'] <= end_time)].iloc[:, 1:]
-            df_spectrogram_subsample = df_spectrogram_subsample.fillna(0)
+            spectrogram = get_spectrogram(df_spectrogram, start_time, fill_na=True, log_scale=False)
             spectrogram_file_path = dataset_directory / 'spectrograms' / f'{spectrogram_id}_{spectrogram_sub_id}.npy'
-
-            nan_counts = df_spectrogram_subsample.isnull().sum().to_dict()
-            nan_counts = {f'{k}_nan_count': v for k, v in nan_counts.items()}
-
-            metadata_dict = {
-                'spectrogram_id': spectrogram_id,
-                'spectrogram_sub_id': spectrogram_sub_id,
-                'row_count': df_spectrogram_subsample.shape[0],
-                'column_count': df_spectrogram_subsample.shape[1]
-            }
-            metadata_dict.update(nan_counts)
-            metadata.append(metadata_dict)
-
-            if channels:
-                spectrogram = np.stack([
-                    df_spectrogram_subsample[left_temporal_chain].values,
-                    df_spectrogram_subsample[right_temporal_chain].values,
-                    df_spectrogram_subsample[left_parasagittal_chain].values,
-                    df_spectrogram_subsample[right_parasagittal_chain].values
-                ], axis=-1).transpose(1, 0, 2)
-            else:
-                spectrogram = df_spectrogram_subsample.values.T
-
             np.save(spectrogram_file_path, spectrogram)
 
     df_metadata = pd.DataFrame(metadata)
