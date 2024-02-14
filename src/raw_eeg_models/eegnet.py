6666b778e6805a19c60db1017c6b980e55c57f29
gunesevitan <gunesevitan@gmail.com>
2024-02-14 20:27:29 +0300

    eegnet

--- src/raw_eeg_models/eegnet.py
+++ src/raw_eeg_models/eegnet.py
@@ -4,40 +4,17 @@ import torch.nn as nn
 from heads import ClassificationHead
 
 
-class GatedConv1d(nn.Module):
-
-    def __init__(self, in_channels, out_channels, kernel_size=(1,), stride=(1,), padding=(2,), padding_mode='replicate'):
-
-        super(GatedConv1d, self).__init__()
-
-        self.conv = nn.Sequential(
-            nn.Conv1d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding, padding_mode=padding_mode, bias=True),
-            nn.BatchNorm1d(out_channels),
-            nn.LeakyReLU(inplace=True)
-        )
-
-        self.gate = nn.Sequential(
-            nn.Conv1d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding, padding_mode=padding_mode, bias=True),
-            nn.Sigmoid()
-        )
-
-    def forward(self, x):
-
-        return self.conv(x) * self.gate(x)
-
-
 class ResNet1DBlock(nn.Module):
 
     def __init__(self, in_channels, out_channels, kernel_size, stride, padding, downsampling):
 
         super(ResNet1DBlock, self).__init__()
 
+        self.conv1 = nn.Conv1d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size, stride=stride, padding=padding, bias=True)
         self.bn1 = nn.BatchNorm1d(num_features=in_channels)
-        self.relu = nn.LeakyReLU(inplace=False)
-        self.dropout = nn.Dropout(p=0.0, inplace=False)
-        self.conv1 = nn.Conv1d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size, stride=stride, padding=padding, bias=False)
+        self.activation = nn.LeakyReLU(inplace=False)
+        self.conv2 = nn.Conv1d(in_channels=out_channels, out_channels=out_channels, kernel_size=kernel_size, stride=stride, padding=padding, bias=True)
         self.bn2 = nn.BatchNorm1d(num_features=out_channels)
-        self.conv2 = nn.Conv1d(in_channels=out_channels, out_channels=out_channels, kernel_size=kernel_size, stride=stride, padding=padding, bias=False)
         self.maxpool = nn.MaxPool1d(kernel_size=2, stride=2, padding=0)
         self.downsampling = downsampling
 
@@ -45,59 +22,59 @@ class ResNet1DBlock(nn.Module):
 
         identity = x
 
-        x = self.bn1(x)
-        x = self.relu(x)
-        x = self.dropout(x)
         x = self.conv1(x)
-        x = self.bn2(x)
-        x = self.relu(x)
-        x = self.dropout(x)
+        x = self.bn1(x)
+        x = self.activation(x)
+
         x = self.conv2(x)
+        x = self.bn2(x)
         x = self.maxpool(x)
 
         identity = self.downsampling(identity)
         outputs = x + identity
+        outputs = self.activation(outputs)
 
         return outputs
 
 
 class EEGNet(nn.Module):
 
-    def __init__(self, kernels, in_channels, fixed_kernel_size, head_args):
+    def __init__(self, in_channels, kernels, fixed_kernel_size, head_args):
 
         super(EEGNet, self).__init__()
 
-        self.kernels = kernels
-        self.planes = 24
-        self.parallel_conv = nn.ModuleList()
         self.in_channels = in_channels
+        self.kernels = kernels
+        self.planes = 128
+        self.parallel_conv = nn.ModuleList()
 
-        for i, kernel_size in enumerate(list(self.kernels)):
-            sep_conv = nn.Conv1d(in_channels=in_channels, out_channels=self.planes, kernel_size=kernel_size, stride=1, padding=0, bias=False)
+        for i, kernel_size in enumerate(self.kernels):
+            sep_conv = nn.Conv1d(in_channels=in_channels, out_channels=self.planes, kernel_size=kernel_size, stride=1, padding=0, bias=True)
             self.parallel_conv.append(sep_conv)
 
         self.bn1 = nn.BatchNorm1d(num_features=self.planes)
-        self.relu = nn.ReLU(inplace=False)
-        self.conv1 = nn.Conv1d(in_channels=self.planes, out_channels=self.planes, kernel_size=fixed_kernel_size, stride=2, padding=2, bias=False)
+        self.activation = nn.LeakyReLU(inplace=False)
+        self.conv1 = nn.Conv1d(in_channels=self.planes, out_channels=self.planes, kernel_size=fixed_kernel_size, stride=2, padding=2, bias=True)
         self.block = self._make_resnet_layer(kernel_size=fixed_kernel_size, stride=1, padding=fixed_kernel_size // 2)
         self.bn2 = nn.BatchNorm1d(num_features=self.planes)
         self.avgpool = nn.AvgPool1d(kernel_size=6, stride=6, padding=2)
-        self.head = ClassificationHead(input_dimensions=224, **head_args)
+        self.head = ClassificationHead(input_dimensions=256, **head_args)
 
-    def _make_resnet_layer(self, kernel_size, stride, blocks=9, padding=0):
+    def _make_resnet_layer(self, kernel_size, stride, blocks=11, padding=0):
 
         layers = []
 
         for i in range(blocks):
             downsampling = nn.MaxPool1d(kernel_size=2, stride=2, padding=0)
-            layers.append(ResNet1DBlock(
+            res_block = ResNet1DBlock(
                 in_channels=self.planes,
                 out_channels=self.planes,
                 kernel_size=kernel_size,
                 stride=stride,
                 padding=padding,
                 downsampling=downsampling
-            ))
+            )
+            layers.append(res_block)
 
         return nn.Sequential(*layers)
 
@@ -111,16 +88,15 @@ class EEGNet(nn.Module):
 
         out = torch.cat(out_sep, dim=2)
         out = self.bn1(out)
-        out = self.relu(out)
+        out = self.activation(out)
         out = self.conv1(out)
 
         out = self.block(out)
         out = self.bn2(out)
-        out = self.relu(out)
+        out = self.activation(out)
         out = self.avgpool(out)
 
         out = out.reshape(out.shape[0], -1)
-
         result = self.head(out)
 
         return result
