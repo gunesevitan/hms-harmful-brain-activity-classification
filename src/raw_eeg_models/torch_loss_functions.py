01e7613b0dccc148cdc8f1e1c745fd1ea59626b3
gunesevitan <gunesevitan@gmail.com>
2024-02-20 21:06:44 +0300

    label smoothibng

--- src/raw_eeg_models/torch_loss_functions.py
+++ src/raw_eeg_models/torch_loss_functions.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.nn.modules.loss import _WeightedLoss
@@ -31,6 +32,36 @@ class WeightedKLDivWithLogitsLoss(_WeightedLoss):
         inputs = nn.functional.log_softmax(inputs, dim=1)
         loss = F.kl_div(inputs, targets, reduction='none')
         loss = loss * self.weight
+
+        if self.reduction == 'mean':
+            loss = loss.mean()
+        elif self.reduction == 'sum':
+            loss = loss.sum()
+
+        return loss
+
+
+class LabelSmoothingBCEWithLogitsLoss(_WeightedLoss):
+
+    def __init__(self, weight=None, smoothing_factor=0.0, reduction='mean'):
+
+        super(LabelSmoothingBCEWithLogitsLoss, self).__init__(weight=weight, reduction=reduction)
+
+        self.smoothing_factor = smoothing_factor
+        self.weight = weight
+        self.reduction = reduction
+
+    def _smooth_labels(self, targets):
+
+        with torch.no_grad():
+            targets = targets * (1.0 - self.smoothing_factor) + 0.5 * self.smoothing_factor
+
+        return targets
+
+    def forward(self, inputs, targets):
+
+        targets = self._smooth_labels(targets)
+        loss = F.binary_cross_entropy_with_logits(inputs, targets, self.weight)
 
         if self.reduction == 'mean':
             loss = loss.mean()
