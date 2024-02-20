2d4b73cc7675a84d01c50674a58290566abdc2a3
gunesevitan <gunesevitan@gmail.com>
2024-02-20 21:06:55 +0300

    modules

--- src/raw_eeg_models/torch_modules.py
+++ src/raw_eeg_models/torch_modules.py
@@ -1,6 +1,6 @@
 from torch.nn import *
 
-from torch_loss_functions import KLDivWithLogitsLoss, WeightedKLDivWithLogitsLoss
+from torch_loss_functions import KLDivWithLogitsLoss, WeightedKLDivWithLogitsLoss, LabelSmoothingBCEWithLogitsLoss
 from eegnet import EEGNet
 from wavenet import WaveNet
 from eeg_2d_models import EfficientNet
