289c93f1fa94fb7d7b60c63d4638a34ed23389e9
gunesevitan <gunesevitan@gmail.com>
2024-02-24 14:11:36 +0300

    dataset

--- src/raw_eeg_models/torch_datasets.py
+++ src/raw_eeg_models/torch_datasets.py
@@ -10,20 +10,29 @@ class EEGDataset(Dataset):
     def __init__(
             self,
             eeg_paths, targets, target_classes, sample_qualities,
+            interpolate_center=True, fill_edge_value=0, clip_bounds=(-10000, 10000),
             transforms=None,
             stationary_period_random_subsample_probability=0.,
-            mixup_alpha=2, mixup_probability=0., mixup_center_probability=0.
+            mixup_alpha=2, mixup_probability=0., mixup_center_probability=0.,
+            cutmix_probability=0., cutmix_center_probability=0.,
     ):
 
         self.eeg_paths = eeg_paths
         self.targets = targets
         self.target_classes = target_classes
         self.sample_qualities = sample_qualities
+
+        self.interpolate_center = interpolate_center
+        self.fill_edge_value = fill_edge_value
+        self.clip_bounds = clip_bounds
+
         self.transforms = transforms
         self.stationary_period_random_subsample_probability = stationary_period_random_subsample_probability
         self.mixup_alpha = mixup_alpha
         self.mixup_probability = mixup_probability
         self.mixup_center_probability = mixup_center_probability
+        self.cutmix_probability = cutmix_probability
+        self.cutmix_center_probability = cutmix_center_probability
 
     def __len__(self):
 
@@ -77,9 +86,12 @@ class EEGDataset(Dataset):
         else:
             eeg_path = self.eeg_paths[idx]
 
-        eeg = np.load(eeg_path)
-        eeg = pd.DataFrame(eeg).interpolate(method='linear', limit_area='inside')
-        eeg = eeg.fillna(0).values
+        eeg = read_eeg(
+            eeg_path=eeg_path,
+            interpolate_center=self.interpolate_center,
+            fill_edge_value=self.fill_edge_value,
+            clip_bounds=self.clip_bounds
+        )
 
         if self.targets is not None:
             targets = self.targets[idx]
@@ -104,9 +116,12 @@ class EEGDataset(Dataset):
             # Randomly select from the subsample paths
             mixup_idx = np.random.randint(0, len(self.eeg_paths))
 
-            mixup_eeg = np.load(self.eeg_paths[mixup_idx])
-            mixup_eeg = pd.DataFrame(mixup_eeg).interpolate(method='linear', limit_area='inside')
-            mixup_eeg = mixup_eeg.fillna(0).values
+            mixup_eeg = read_eeg(
+                eeg_path=self.eeg_paths[mixup_idx],
+                interpolate_center=self.interpolate_center,
+                fill_edge_value=self.fill_edge_value,
+                clip_bounds=self.clip_bounds
+            )
 
             # Sample MixUp lambda from beta distribution
             mixup_lambda = np.clip(np.random.beta(self.mixup_alpha, self.mixup_alpha), a_min=0.4, a_max=0.6)
@@ -126,12 +141,85 @@ class EEGDataset(Dataset):
                 mixup_sample_quality = self.sample_qualities[mixup_idx]
                 sample_quality = torch.ceil((sample_quality + mixup_sample_quality) / 2)
 
+        if np.random.rand() < self.cutmix_probability:
+
+            # Randomly select from the subsample paths
+            cutmix_idx = np.random.randint(0, len(self.eeg_paths))
+
+            cutmix_eeg = read_eeg(
+                eeg_path=self.eeg_paths[cutmix_idx],
+                interpolate_center=self.interpolate_center,
+                fill_edge_value=self.fill_edge_value,
+                clip_bounds=self.clip_bounds
+            )
+
+            if np.random.rand() < self.cutmix_center_probability:
+                # Apply CutMix to center 10 seconds
+                eeg[4000:6000, :] = cutmix_eeg[4000:6000, :]
+
+                # Replace other values as well since the center 10 seconds is altered
+                if self.targets is not None:
+                    targets = self.targets[cutmix_idx]
+                    targets = torch.as_tensor(targets, dtype=torch.float)
+
+                if self.sample_qualities is not None:
+                    sample_quality = self.sample_qualities[cutmix_idx]
+                    sample_quality = torch.as_tensor(sample_quality, dtype=torch.float)
+
+            else:
+                # Apply CutMix to randomly selected non-center 10 seconds
+                cutmix_seconds = np.random.choice([0, 1, 3, 4])
+                cutmix_start_idx = cutmix_seconds * 2000
+                cutmix_end_idx = (cutmix_seconds + 1) * 2000
+                eeg[cutmix_start_idx:cutmix_end_idx, :] = cutmix_eeg[cutmix_start_idx:cutmix_end_idx, :]
+
         if self.transforms is not None:
             eeg = self.transforms(image=eeg)['image'].float()
         else:
             eeg = torch.as_tensor(eeg, dtype=torch.float)
 
         return eeg, targets, target_class, sample_quality
+
+
+def read_eeg(eeg_path, interpolate_center=True, fill_edge_value=0, clip_bounds=(-10000, 10000)):
+
+    """
+    Read EEG and preprocess it
+
+    Parameters
+    ----------
+    eeg_path: str or pathlib.Path
+        Path of the EEG file
+
+    interpolate_center: bool
+        Whether to interpolate missing values located at center or not
+
+    fill_edge_value: int, float or None
+        Value to fill missing values located at the edges
+
+    clip_bounds: tuple or None
+        Lower and upper bound for clipping valus
+
+    Returns
+    -------
+    eeg: numpy.ndarray of shape (channel, time)
+        Tensor of EEG
+    """
+
+    eeg = pd.DataFrame(np.load(eeg_path))
+
+    if interpolate_center:
+        eeg = eeg.interpolate(method='linear', limit_area='inside')
+
+    if fill_edge_value is not None:
+        eeg = eeg.fillna(0)
+
+    if clip_bounds is not None:
+        eeg = eeg.clip(clip_bounds[0], clip_bounds[1])
+
+    eeg = eeg.values
+
+    return eeg
 
 
 def prepare_data(df, eeg_dataset_path):
