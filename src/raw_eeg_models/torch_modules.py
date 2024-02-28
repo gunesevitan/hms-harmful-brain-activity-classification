c5402669a89cfbcff60d7e775e11f33ea0530b58
gunesevitan <gunesevitan@gmail.com>
2024-02-28 21:15:28 +0300

    csn module

--- src/raw_eeg_models/torch_modules.py
+++ src/raw_eeg_models/torch_modules.py
@@ -3,4 +3,5 @@ from torch.nn import *
 from torch_loss_functions import KLDivWithLogitsLoss, WeightedKLDivWithLogitsLoss, LabelSmoothingBCEWithLogitsLoss
 from eegnet import EEGNet
 from wavenet import WaveNet
-from eeg_2d_models import EfficientNet, CoaT, MaxViT
+from timm_models import EfficientNet, NFNet, CoaT, GCViT, CoAtNet, NextViT, SwinTransformer
+from csn import CSNModel
