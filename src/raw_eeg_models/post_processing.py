b97f7c787a648628ee003a4f6cda862f8a1d8b14
gunesevitan <gunesevitan@gmail.com>
2024-03-04 09:47:02 +0300

    convnext model 0.28x

--- src/raw_eeg_models/post_processing.py
+++ src/raw_eeg_models/post_processing.py
@@ -1,6 +1,5 @@
 import sys
 import json
-import numpy as np
 import pandas as pd
 
 import preprocessing
@@ -44,21 +43,20 @@ if __name__ == '__main__':
     df = preprocessing.extract_sample_quality(df=df, target_columns=target_columns)
     df = preprocessing.normalize_targets(df=df, target_columns=target_columns)
 
-    raw_eeg_1d_eegnet_directory = 'raw_eeg_1d_eegnet_19x10000_quality_2'
     raw_eeg_2d_efficientnetv2medium_directory = 'raw_eeg_2d_efficientnetv2medium_384x512_quality_2'
-    raw_eeg_2d_coatlitemedium_directory = 'raw_eeg_2d_coatlitemedium_384x512_ekg_quality_2'
+    raw_eeg_2d_convnextbase_directory = 'raw_eeg_2d_convnextbase_384x512_quality_2'
+    raw_eeg_2d_coatlitemedium_directory = 'raw_eeg_2d_coatlitemedium_384x512_quality_2'
     raw_eeg_2d_nextvitlarge_directory = 'raw_eeg_2d_nextvitlarge_384x512_quality_2'
-    raw_eeg_2d_maxvittiny_directory = 'raw_eeg_2d_maxvittiny_384x512_ekg_quality_2'
-    raw_eeg_2d_swinsmall_directory = 'raw_eeg_2d_swinsmall_384x512_ekg_quality_2'
-    raw_eeg_3d_resnet50csn_directory = 'raw_eeg_3d_resnet50csn_5x96x512_quality_2'
+    raw_eeg_2d_maxvittiny_directory = 'raw_eeg_2d_maxvittiny_384x512_quality_2'
+    raw_eeg_2d_swinsmall_directory = 'raw_eeg_2d_swinsmall_384x512_quality_2'
 
     prediction_columns = [f'{column}_prediction' for column in target_columns]
     columns_to_merge = ['eeg_id', 'eeg_sub_id', 'sample_quality']
 
-    df_raw_eeg_1d_eegnet = pd.read_csv(settings.MODELS / raw_eeg_1d_eegnet_directory / 'oof_predictions.csv')
-    df_raw_eeg_1d_eegnet = df_raw_eeg_1d_eegnet.merge(df.loc[:, columns_to_merge], on=columns_to_merge[:-1], how='left')
     df_raw_eeg_2d_efficientnetv2medium = pd.read_csv(settings.MODELS / raw_eeg_2d_efficientnetv2medium_directory / 'oof_predictions.csv')
     df_raw_eeg_2d_efficientnetv2medium = df_raw_eeg_2d_efficientnetv2medium.merge(df.loc[:, columns_to_merge], on=columns_to_merge[:-1], how='left')
+    df_raw_eeg_2d_convnextbase = pd.read_csv(settings.MODELS / raw_eeg_2d_convnextbase_directory / 'oof_predictions.csv')
+    df_raw_eeg_2d_convnextbase = df_raw_eeg_2d_convnextbase.merge(df.loc[:, columns_to_merge], on=columns_to_merge[:-1], how='left')
     df_raw_eeg_2d_coatlitemedium = pd.read_csv(settings.MODELS / raw_eeg_2d_coatlitemedium_directory / 'oof_predictions.csv')
     df_raw_eeg_2d_coatlitemedium = df_raw_eeg_2d_coatlitemedium.merge(df.loc[:, columns_to_merge], on=columns_to_merge[:-1], how='left')
     df_raw_eeg_2d_nextvitlarge = pd.read_csv(settings.MODELS / raw_eeg_2d_nextvitlarge_directory / 'oof_predictions.csv')
@@ -67,26 +65,22 @@ if __name__ == '__main__':
     df_raw_eeg_2d_maxvittiny = df_raw_eeg_2d_maxvittiny.merge(df.loc[:, columns_to_merge], on=columns_to_merge[:-1], how='left')
     df_raw_eeg_2d_swinsmall = pd.read_csv(settings.MODELS / raw_eeg_2d_swinsmall_directory / 'oof_predictions.csv')
     df_raw_eeg_2d_swinsmall = df_raw_eeg_2d_swinsmall.merge(df.loc[:, columns_to_merge], on=columns_to_merge[:-1], how='left')
-    df_raw_eeg_3d_resnet50csn = pd.read_csv(settings.MODELS / raw_eeg_3d_resnet50csn_directory / 'oof_predictions.csv')
-    df_raw_eeg_3d_resnet50csn = df_raw_eeg_3d_resnet50csn.merge(df.loc[:, columns_to_merge], on=columns_to_merge[:-1], how='left')
 
     prediction_dfs = [
-        df_raw_eeg_1d_eegnet,
         df_raw_eeg_2d_efficientnetv2medium,
+        df_raw_eeg_2d_convnextbase,
         df_raw_eeg_2d_coatlitemedium,
         df_raw_eeg_2d_nextvitlarge,
         df_raw_eeg_2d_maxvittiny,
-        df_raw_eeg_2d_swinsmall,
-        df_raw_eeg_3d_resnet50csn
+        df_raw_eeg_2d_swinsmall
     ]
     model_names = [
-        'raw_eeg_1d_eegnet_19x10000_quality_2',
         'raw_eeg_2d_efficientnetv2medium_384x512_quality_2',
+        'raw_eeg_2d_convnextbase_384x512_quality_2',
         'raw_eeg_2d_coatlitemedium_384x512_quality_2',
         'raw_eeg_2d_nextvitlarge_384x512_quality_2',
         'raw_eeg_2d_maxvittiny_384x512_quality_2',
         'raw_eeg_2d_swinsmall_384x512_quality_2',
-        'raw_eeg_3d_resnet50csn_5x96x512_quality_2'
     ]
     for model_name, df in zip(model_names, prediction_dfs):
         oof_scores = metrics.multiclass_classification_scores(
@@ -104,13 +98,12 @@ if __name__ == '__main__':
         settings.logger.info(f'{model_name} OOF Scores: {json.dumps(oof_scores, indent=2)}\n')
 
     df_blend = df_raw_eeg_2d_efficientnetv2medium.copy(deep=True)
-    df_blend[prediction_columns] = (df_raw_eeg_1d_eegnet[prediction_columns] * 0.).values + \
-                                   (df_raw_eeg_2d_efficientnetv2medium[prediction_columns] * 0.2).values + \
-                                   (df_raw_eeg_2d_coatlitemedium[prediction_columns] * 0.2).values + \
-                                   (df_raw_eeg_2d_nextvitlarge[prediction_columns] * 0.2).values + \
-                                   (df_raw_eeg_2d_maxvittiny[prediction_columns] * 0.2).values + \
-                                   (df_raw_eeg_2d_swinsmall[prediction_columns] * 0.2).values + \
-                                   (df_raw_eeg_3d_resnet50csn[prediction_columns] * 0.).values
+    df_blend[prediction_columns] = (df_raw_eeg_2d_efficientnetv2medium[prediction_columns] * 0.166).values + \
+                                   (df_raw_eeg_2d_convnextbase[prediction_columns] * 0.17).values + \
+                                   (df_raw_eeg_2d_coatlitemedium[prediction_columns] * 0.166).values + \
+                                   (df_raw_eeg_2d_nextvitlarge[prediction_columns] * 0.166).values + \
+                                   (df_raw_eeg_2d_maxvittiny[prediction_columns] * 0.166).values + \
+                                   (df_raw_eeg_2d_swinsmall[prediction_columns] * 0.166).values
 
     df_blend = normalize_probabilities(df_blend, prediction_columns)
     oof_scores = metrics.multiclass_classification_scores(
