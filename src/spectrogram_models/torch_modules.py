d80d73609bfd1c4e99378a1cdeed2c31153d29af
gunesevitan <gunesevitan@gmail.com>
2024-03-04 09:52:14 +0300

    modules

---
+++ src/spectrogram_models/torch_modules.py
@@ -0,0 +1,4 @@
+from torch.nn import *
+
+from torch_loss_functions import KLDivWithLogitsLoss, WeightedKLDivWithLogitsLoss, LabelSmoothingBCEWithLogitsLoss
+from timm_models import EfficientNet, NFNet, ConvNeXt, CoaT, GCViT, CoAtNet, NextViT, SwinTransformer
