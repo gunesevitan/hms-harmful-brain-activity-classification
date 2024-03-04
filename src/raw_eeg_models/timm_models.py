cbf73541eb6cbff309b0978523298a9360d8b4a0
gunesevitan <gunesevitan@gmail.com>
2024-03-04 09:47:34 +0300

    convnext

--- src/raw_eeg_models/timm_models.py
+++ src/raw_eeg_models/timm_models.py
@@ -83,6 +83,44 @@ class NFNet(nn.Module):
         return output
 
 
+class ConvNeXt(nn.Module):
+
+    def __init__(self, model_name, pretrained, backbone_args, pooling_type, dropout_rate, head_args):
+
+        super(ConvNeXt, self).__init__()
+
+        self.backbone = timm.create_model(
+            model_name=model_name,
+            pretrained=pretrained,
+            **backbone_args
+        )
+
+        input_features = self.backbone.get_classifier().in_features
+
+        self.pooling_type = pooling_type
+        self.dropout = nn.Dropout(dropout_rate) if dropout_rate > 0 else nn.Identity()
+        self.head = ClassificationHead(input_dimensions=input_features, **head_args)
+
+    def forward(self, x):
+
+        x = self.backbone.forward_features(x)
+
+        if self.pooling_type == 'avg':
+            x = F.adaptive_avg_pool2d(x, output_size=(1, 1)).view(x.size(0), -1)
+        elif self.pooling_type == 'max':
+            x = F.adaptive_max_pool2d(x, output_size=(1, 1)).view(x.size(0), -1)
+        elif self.pooling_type == 'concat':
+            x = torch.cat([
+                F.adaptive_avg_pool2d(x, output_size=(1, 1)).view(x.size(0), -1),
+                F.adaptive_max_pool2d(x, output_size=(1, 1)).view(x.size(0), -1)
+            ], dim=-1)
+
+        x = self.dropout(x)
+        output = self.head(x)
+
+        return output
+
+
 class CoaT(nn.Module):
 
     def __init__(self, model_name, pretrained, backbone_args, dropout_rate, head_args):
